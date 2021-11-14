6a6577546f49b11702dc99c9f9165367137e9f40
Isa <isa@aboutbirdie.com>
2021-11-14 01:04:41 +0100

    navigation: creating react router

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -12,7 +12,9 @@ const Dropdown = ({ options, selected, onSelectedChange, label }) => {
     };
     document.body.addEventListener("click", onBodyClick, { capture: true });
     return () => {
-      document.body.removeEventListener("click", onBodyClick);
+      document.body.removeEventListener("click", onBodyClick, {
+        capture: true,
+      });
     };
   }, []);
   const renderedOptions = options.map((option) => {
